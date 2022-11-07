16a81a6dbc54bdcd67e79d7e280783d5e6b238c5
vrushalitarte01 <tarte.v@northeastern.edu>
2022-11-06 23:47:51 -0500

    a7 Redux fa22

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,22 +1,37 @@
 import React from "react";
 import ExploreComponent from "./explore";
+import HomeComponent from "./home/index.js";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
+import {Routes, Route} from "react-router";
+import whoReducer
+    from "./reducers/who-reducer";
+import tuitsReducer from "./reducers/tuits-reducer";
+import {configureStore}
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
     return (
+        <Provider store={store}>
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="Explore"/>
+                <NavigationSidebar active="Home"/>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
-                <ExploreComponent/>
+                <Routes>
+                    <Route index element={<HomeComponent />} />
+                    <Route path="explore" element={<ExploreComponent />} />
+                </Routes>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
+        </Provider>
     );
 }
 
